0ba9dbd48c1a5c921815273c607f5efb135bc8a7
K31NER <zunigaromerokeiner@gmail.com>
2025-06-26 18:13:59 -0500

    feat: Se completo el proceso de ETL

--- ETL/Extract.py
+++ ETL/Extract.py
@@ -1,3 +1,4 @@
+import re
 import time
 from playwright.sync_api import sync_playwright
 
@@ -5,7 +6,8 @@ from playwright.sync_api import sync_playwright
 URL: str = "https://www.fincaraiz.com.co/venta/pagina"
 
 # Definimos las listas
-titulos,enlaces,precios,habitaciones,baños,metros = [],[],[],[],[],[]
+titulos,enlaces,precios,habitaciones_list,baños_list,metros_list = [],[],[],[],[],[]
+
 
 def scrapear(N_pagina: int = 1) -> list:
     """ Scrapea los datos de fincaraiz de x nuemero de paginas
@@ -56,15 +58,34 @@ def scrapear(N_pagina: int = 1) -> list:
                 titulo = link.get_attribute("title") if link else "N/A"
                 href = link.get_attribute("href") if link else "N/A"
                 
+                # Inicializamos por cada propiedad
+                ambientes = 0
+                habitaciones = 0
+                baños = 0
+                metros = 0
+                
                 # Procesamos el bloque de detalles si existe
                 if detalles:
                     texto = detalles.inner_text()
-                    try:
-                        habitaciones_data = texto.split("Habs.")[0].strip()
-                        baños_data = texto.split("Habs.")[1].split("Baños")[0].strip()
-                        metros_data = texto.split("Baños")[1].replace("m²", "").strip()
-                    except (IndexError, AttributeError):
-                        print("⚠ No se pudo parsear correctamente los detalles, verificar estructura.")
+                    
+                    # Buscamos los valores en base a sus palabras clave
+                    match_ambiente = re.search(r"(\d+)\s+Ambiente", texto)
+                    match_habs = re.search(r"(\d+)\s*Habs?", texto)
+                    match_banos = re.search(r"(\d+)\s*Baños?", texto)
+                    match_metros = re.search(r"(\d+(\.\d+)?)\s*m²", texto) 
+
+                    if match_ambiente:
+                        ambientes = match_ambiente.group(1) # Devolvemos el valor
+                    if match_habs:
+                        habitaciones = match_habs.group(1)
+                    if match_banos:
+                        baños = match_banos.group(1)
+                    if match_metros:
+                        metros = match_metros.group(1)
+
+                # Consolidación
+                habitaciones_final = habitaciones if habitaciones != "N/A" else ambientes
+                
                 """ 
                 # Imprimimos o guardamos los resultados
                 print(f"Título: {titulo}")
@@ -79,23 +100,25 @@ def scrapear(N_pagina: int = 1) -> list:
                 titulos.append(titulo)
                 enlaces.append(f"https://www.fincaraiz.com.co{href}")
                 precios.append(precio_limpio)
-                habitaciones.append(habitaciones_data)
-                baños.append(baños_data)
-                metros.append(metros_data)
+                habitaciones_list.append(habitaciones_final)
+                baños_list.append(baños)
+                metros_list.append(metros)
     
         browser.close()
         final = time.perf_counter() - inicio
+    
+    # Preparamos los datos
     data = {
         "titulos": titulos,
         "enlaces": enlaces,
         "precios": precios,
-        "habitaciones": habitaciones,
-        "baños": baños,
-        "metros_cuadrados": metros
+        "habitaciones": habitaciones_list,
+        "baños": baños_list,
+        "metros_cuadrados": metros_list
     }
     return [final/60 , data]
     
 if __name__ == "__main__":
-    resultado = scrapear(10)
+    resultado = scrapear(50)
     print(f"tiempo{resultado[0]}")
     print(f" Data: {resultado[1]}")
