70b1ee09277088ce0b5421e18145428ab4259f6f
K31NER <zunigaromerokeiner@gmail.com>
2025-07-05 18:41:55 -0500

    feat: Se agrego la conexion con la api
    Se realizo la conexion con la api de fastapi mediante un formulario que esta en un modal

--- ML/main.py
+++ ML/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
-from schema import Testinput
+from schema import Datainput
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="API InmoPipeline",
     version="0.1",
     summary="Expone el modelo de machine learning para predicción de precios inmobiliarios",
+)
+
+origins = [
+    "http://localhost:8501"
+]
+
+# Agregamos los corns para indicar que streamlit puede realizar solicitudes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods= ["*"],
+    allow_headers = ["*"]
 )
 
 @app.get("/")
@@ -14,7 +28,7 @@ async def index():
     
     
 @app.post("/model")
-async def run_model(data: Testinput):
+async def run_model(data: Datainput):
     """
     Endpoint que recibe los datos de entrada y devuelve la predicción de precio.
     """
@@ -24,7 +38,6 @@ async def run_model(data: Testinput):
     
     # Mientras tanto, respuesta de ejemplo:
     precio_estimado = "1.000M COP (estimado ficticio)"
-    
     return JSONResponse(content={
         "Prediccion": precio_estimado,
         "Datos Recibidos": data.model_dump()
