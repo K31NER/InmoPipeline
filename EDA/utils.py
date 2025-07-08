1ba6c4f90ea0db8cb25e3d1cb71ca1ffda568f96
K31NER <zunigaromerokeiner@gmail.com>
2025-07-07 23:57:13 -0500

    fix: Se corriguio la obtencion del csv con los datos

--- EDA/utils.py
+++ EDA/utils.py
@@ -1,5 +1,14 @@
 import httpx
+import pandas as pd
 import streamlit as st
+from pathlib import Path
+
+@st.cache_data()
+def get_data():
+    current_dir = Path(__file__).resolve().parent  # EDA/
+    data_path = current_dir.parent / 'Data' / 'propiedades.csv'
+    df = pd.read_csv(data_path)
+    return df
 
 # Definimos la url donde esta nuestro modelo
 URL_MODEL = "http://127.0.0.1:8000/model"
