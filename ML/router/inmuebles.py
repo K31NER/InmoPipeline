e147410debbee1719af8566afabda9d29d32555e
K31NER <zunigaromerokeiner@gmail.com>
2025-07-09 23:57:15 -0500

    fix: Correciones en lso endpoints de obtener datos por ciudad y region

--- ML/router/inmuebles.py
+++ ML/router/inmuebles.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import unicodedata
 from sqlmodel import text
 from ML.db import connection
@@ -7,7 +8,7 @@ from fastapi.responses import JSONResponse
 router = APIRouter(tags=["Datos"])
 
 # Definimos la consulta base
-query_base = "SELECT ciudad_normalizada,region,precios,habitaciones,baños,metros_cuadrados,enlaces FROM propiedades "
+query_base = "SELECT ciudades,region,precios,habitaciones,baños,metros_cuadrados,enlaces FROM propiedades "
 
 # Diccionario para mapear entrada normalizada a nombre correcto
 regiones_map = {
@@ -24,8 +25,14 @@ def normalizar_texto(texto: str) -> str:
     # Elimina tildes y pone en minúsculas
     return unicodedata.normalize("NFKD", texto).encode("ascii", "ignore").decode().lower()
 
-@router.get("/data_by_region/{region}",summary="Retorna todos los datos de los inmuebles en esa region")
-def get_data(db:connection,region:str,limite:int = None):
+@router.get("/propiedades_region/{region}",summary="Retorna todos los datos de los inmuebles en esa region")
+def get_propiedades_by_region(db:connection,
+                            region:str,habs:int = None,
+                            limite:int = None):
+    
+    # Creamos una lista y un diccionario para menajar los parametros
+    filtros = []
+    params = {}
     
     #limpiamos la region
     region_normalizada = normalizar_texto(region)
@@ -36,9 +43,28 @@ def get_data(db:connection,region:str,limite:int = None):
                         detail={"details":f"Region {region} no encontrada",
                                 "Lista de regiones": list(regiones_map.keys())})
         
+    # Agregamos el filtro principal
+    filtros.append("region = :region")
+    params["region"] = region_map
+    
+    # Validamos las habitaciones
+    if habs:
+        filtros.append("CAST(habitaciones AS INTEGER) = :habs")
+        params["habs"] = habs
+        
+    # Definimos el filtro where
+    where_clause = " WHERE " + " AND ".join(filtros) if filtros else ""
+    
+    # Definimos el limitador
+    limit_clause = " LIMIT :limit" if limite else ""
+    
+    # Validamos los limites
+    if limite:
+        params["limit"] = limite
+        
     try:
-        query = text(f"{query_base} WHERE region = :region LIMIT :limite")
-        resultado = db.exec(query.params(region = region_map,limite = limite))
+        query = text(f"{query_base}{where_clause}{limit_clause}")
+        resultado = db.exec(query.params(**params))
         
         rows = resultado.fetchall()
         datos = [dict(row._mapping) for row in rows]
@@ -47,21 +73,44 @@ def get_data(db:connection,region:str,limite:int = None):
     
     return JSONResponse(content={
         "size": len(datos),
-        "region":region_map,
+        "filtro": params,
         "data": datos,
     },status_code=200)
 
-@router.get("/data_by_ciudad/{ciudad}",
+@router.get("/propiedades_ciudad/{ciudad}",
             summary="Retorna todos los datos de los inmubeles de esa ciudad")
-def get_data(db:connection,ciudad:str,limite:int = None):
-        
+def get_propiedades_by_ciudad(db:connection,ciudad:str,
+                            habs:int = None, limite:int = None):
+    
+    # Creamos una lista y un diccionario para menajar los parametros
+    
+    filtros = []
+    params = {}
+    
+    # Validamos la ciudad
     if not ciudad:
         raise HTTPException(status_code=404,
                         detail={"details":f"Ciudades no encontradas"})
         
+    # Agregamos la condicion principal
+    filtros.append("ciudades = :ciudad")
+    params["ciudad"] = normalizar_texto(ciudad)
+    
+    # Validamos las habitaciones
+    if habs:
+        filtros.append("CAST(habitaciones AS INTEGER) = :habs")
+        params["habs"] = habs
+    
+    # Creamos las consultas
+    where_clause = " WHERE " + " AND ".join(filtros) if filtros else ""
+    limit_clause = " LIMIT :limit" if limite else ""
+    
+    if limite:
+        params["limit"] = limite
+        
     try:
-        query = text(f"{query_base} WHERE ciudad_normalizada = :ciudad LIMIT :limite")
-        resultado = db.exec(query.params(ciudad = normalizar_texto(ciudad),limite = limite))
+        query = text(f"{query_base}{where_clause}{limit_clause}")
+        resultado = db.exec(query.params(**params))
         
         rows = resultado.fetchall()
         datos = [dict(row._mapping) for row in rows]
@@ -70,6 +119,6 @@ def get_data(db:connection,ciudad:str,limite:int = None):
     
     return JSONResponse(content={
         "size": len(datos),
-        "ciudades":ciudad,
+        "filtros":params,
         "data": datos,
     },status_code=200)
